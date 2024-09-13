8ba1f95cc1811fd787ce59db2c520935fb8c3403
PopovIliia <61333114+PopovIliia@users.noreply.github.com>
2024-09-13 17:03:09 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-import math
+import cmath
 
 app = Flask(__name__)
 
@@ -8,8 +8,14 @@ def index():
     result = None
     if request.method == 'POST':
         try:
-            number = float(request.form['number'])
-            result = math.sqrt(number)
+            # Получаем число из формы
+            number_str = request.form['number']
+            
+            # Преобразуем введенное значение в комплексное число
+            number = complex(number_str)
+            
+            # Вычисляем квадратный корень комплексного числа
+            result = cmath.sqrt(number)
         except ValueError:
             result = "Введите корректное число"
         except Exception as e:
@@ -19,3 +25,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
